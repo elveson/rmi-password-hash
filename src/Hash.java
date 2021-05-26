12dea44139db9e66a5f0df513c2451d8c424e742
Elveson Costa <elveson.esc@gmail.com>
2021-05-26 20:18:53 -0300

    Add file Hash

--- src/Hash.java
+++ src/Hash.java
@@ -1,7 +1,11 @@
+import java.rmi.Remote;
+import java.rmi.RemoteException;
+
 /**
  * @author Elveson S. Costa
  * @created 26/05/2021 - 7:37 PM
  * @project SD_RMI_PasswordHash
  */
-public class Hash {
+public interface Hash extends Remote {
+    public String passwordHash(String s) throws RemoteException;
 }
